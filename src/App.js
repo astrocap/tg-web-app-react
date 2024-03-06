bad5722ea22f1e0cfb8f78813ad240109be17979
astrocap <bykovmax104@gmail.com>
2024-03-06 19:13:46 +0300

    penis

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
-window.Telegram.WebApp.
+const tg = window.Telegram.WebApp;
+
 function App() {
+
+  useEffect(() => {
+    tg.ready();
+  }, [])
+
+  const onClose = () => {
+    tg.close()
+  }
+  
   return (
     <div className="App">
      work 
+      <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
